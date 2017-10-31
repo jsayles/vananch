543a4b1852635e57c37b528e35187eba4425480a
Jacob Sayles <jsayles@gmail.com>
2017-10-31 10:18:27 -0700

    fixed bug in prev/next links

--- vananch/views.py
+++ vananch/views.py
@@ -6,15 +6,16 @@ from vananch.models import Ship, ShipRecord, ShipImport
 from vananch.importer import ShipImporter
 
 
-def map(request, import_id=0):
-    if import_id == 0:
+def map(request, import_id=None):
+    if not import_id:
         ship_import = ShipImport.objects.last()
         import_id = ship_import.id
     else:
+        import_id = int(import_id)
         ship_import = ShipImport.objects.get(id=import_id)
 
-    prev_imports = ShipImport.objects.filter(id__lt=import_id).order_by("id")[3:]
-    next_imports = ShipImport.objects.filter(id__gt=import_id)[:3]
+    prev_imports = ShipImport.objects.filter(id__range=(import_id - 3, import_id))[:3]
+    next_imports = ShipImport.objects.filter(id__range=(import_id, import_id + 3))[1:4]
     records = ShipRecord.objects.filter(ship_import=ship_import)
     context = {
         'prev_imports': prev_imports,
