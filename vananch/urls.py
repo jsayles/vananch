1d5d0b9a357ca6697543cb6b1395818c24599743
Jacob Sayles <jsayles@gmail.com>
2017-10-31 10:21:38 -0700

    added favicon

--- vananch/urls.py
+++ vananch/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from . import views
 
+admin.autodiscover()
+
+favicon_view = RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)
+
 urlpatterns = [
+    url(r'^favicon\.ico$', favicon_view),
+
     url(r'^$', views.map, name='home'),
     url(r'^map/(?P<import_id>\d+)$', views.map, name='map'),
     url(r'^import/$', views.ship_import, name='import'),
+
     url(r'^admin/', admin.site.urls),
 ]
